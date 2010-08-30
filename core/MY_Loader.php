074b49ffa9eb8eb33e92e231eb05aa5056cfe6d6
wiredesignz <wiredesignz@localhost>
2010-08-30 17:35:05 +1200

    Alter require

--- core/MY_Loader.php
+++ core/MY_Loader.php
@@ -1,5 +1,5 @@
 <?php (defined('BASEPATH')) OR exit('No direct script access allowed');
 
-require_once APPPATH."third_party/MX/Loader.php";
+require APPPATH."third_party/MX/Loader.php";
 
 class MY_Loader extends MX_Loader {}
