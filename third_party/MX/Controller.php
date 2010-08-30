586fe8db8d9ea2ff322ccb0b0d7a8b074505bd31
wiredesignz <wiredesignz@localhost>
2010-08-30 12:30:29 +1200

    First import

---
+++ third_party/MX/Controller.php
@@ -0,0 +1,66 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/* load MX core classes */
+require_once 'Lang.php';
+require_once 'Config.php';
+require_once 'Base.php';
+
+/**
+ * Modular Extensions - PHP5
+ *
+ * Adapted from the CodeIgniter Core Classes
+ * @link	http://codeigniter.com
+ *
+ * Description:
+ * This library replaces the CodeIgniter Controller class
+ * and adds features allowing use of modules and the HMVC design pattern.
+ *
+ * Install this file as application/third_party/MX/Controller.php
+ *
+ * @copyright	Copyright (c) Wiredesignz 2010-08-31
+ * @version 	5.3.0
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+class MX_Controller
+{			
+	public $autoload = array();
+	
+	public function __construct() {
+		
+		$class = str_replace($this->config->item('controller_suffix'), '', get_class($this));
+		log_message('debug', $class." MX_Controller Initialized");
+		
+		/* register this controller */
+		Modules::$registry[strtolower($class)] = $this;	
+		
+		/* assign the loader class */
+		$class = $this->config->item('subclass_prefix').'Loader';
+		$this->load = (class_exists($class, FALSE)) ? new $class : new MX_Loader;
+		
+		$this->load->_module();	
+		
+		/* autoload module items */
+		$this->load->_autoloader($this->autoload);
+	}
+	
+	public function __get($var) {
+		return CI::$APP->$var;
+	}
+}
