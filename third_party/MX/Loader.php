88cb9cb0311b46339bdd6d302604721c7ebc8533
wiredesignz <wiredesignz@localhost>
2011-04-25 21:47:39 +1200

    Revert to using public methods to remain backward compatible with CI Core

--- third_party/MX/Loader.php
+++ third_party/MX/Loader.php
@@ -256,9 +256,9 @@ class MX_Loader extends CI_Loader
 		return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $this->_ci_object_to_array($vars), '_ci_return' => $return));
 	}
 
-	protected function _ci_is_instance() {}
+	public function _ci_is_instance() {}
 
-	protected function _ci_get_component($component) {
+	public function _ci_get_component($component) {
 		return CI::$APP->$component;
 	} 
 
@@ -266,7 +266,7 @@ class MX_Loader extends CI_Loader
 		return (isset($this->controller)) ? $this->controller->$class : CI::$APP->$class;
 	}
 
-	protected function _ci_load($_ci_data) {
+	public function _ci_load($_ci_data) {
 		
 		foreach (array('_ci_view', '_ci_vars', '_ci_path', '_ci_return') as $_ci_val) {
 			$$_ci_val = ( ! isset($_ci_data[$_ci_val])) ? FALSE : $_ci_data[$_ci_val];
