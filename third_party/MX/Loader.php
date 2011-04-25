29bfba8c406b6a69665d4c2f7c3e5e4547759150
wiredesignz <wiredesignz@localhost>
2011-04-25 19:47:09 +1200

    Updated to resolve $_ci_cached_vars error

--- third_party/MX/Loader.php
+++ third_party/MX/Loader.php
@@ -38,6 +38,7 @@ class MX_Loader extends CI_Loader
 	private $_module;
 	
 	public $_ci_plugins;
+	public $_ci_cached_vars;
 	
 	public function __construct() {
 		
@@ -208,7 +209,7 @@ class MX_Loader extends CI_Loader
 	}
 
 	/** Load an array of models **/
-	function models($models) {
+	public function models($models) {
 		foreach ($models as $_model) $this->model($_model);	
 	}
 
@@ -255,9 +256,9 @@ class MX_Loader extends CI_Loader
 		return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $this->_ci_object_to_array($vars), '_ci_return' => $return));
 	}
 
-	public function _ci_is_instance() {}
+	protected function _ci_is_instance() {}
 
-	public function _ci_get_component($component) {
+	protected function _ci_get_component($component) {
 		return CI::$APP->$component;
 	} 
 
@@ -265,7 +266,7 @@ class MX_Loader extends CI_Loader
 		return (isset($this->controller)) ? $this->controller->$class : CI::$APP->$class;
 	}
 
-	function _ci_load($_ci_data) {
+	protected function _ci_load($_ci_data) {
 		
 		foreach (array('_ci_view', '_ci_vars', '_ci_path', '_ci_return') as $_ci_val) {
 			$$_ci_val = ( ! isset($_ci_data[$_ci_val])) ? FALSE : $_ci_data[$_ci_val];
