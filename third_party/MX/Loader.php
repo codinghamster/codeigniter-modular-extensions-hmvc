770919fda191d184efc8ce1824a45a3079fc7fd4
wiredesignz <wiredesignz@localhost>
2011-12-15 22:48:28 +1300

    Update MX_Loader to prevent empty view paths

--- third_party/MX/Loader.php
+++ third_party/MX/Loader.php
@@ -254,7 +254,7 @@ class MX_Loader extends CI_Loader
 	/** Load a module view **/
 	public function view($view, $vars = array(), $return = FALSE) {
 		list($path, $view) = Modules::find($view, $this->_module, 'views/');
-		$this->_ci_view_paths = array($path => TRUE) + $this->_ci_view_paths;
+		if ($path != FALSE) $this->_ci_view_paths = array($path => TRUE) + $this->_ci_view_paths;
 		return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $this->_ci_object_to_array($vars), '_ci_return' => $return));
 	}
 
