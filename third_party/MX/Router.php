8caa676223b052d998549b48c5c7376210f67d10
wattana <nami_sayuri@hotmail.com>
2015-08-11 15:34:04 +0700

    check controller suffix before add

--- third_party/MX/Router.php
+++ third_party/MX/Router.php
@@ -235,7 +235,10 @@ class MX_Router extends CI_Router
 
 	public function set_class($class)
 	{
-		$class = $class.$this->config->item('controller_suffix');
-		parent::set_class($class);
+        $suffix = $this->config->item('controller_suffix');
+        if (strpos($class, $suffix) === false) {
+            $class = $class.$suffix;
+        }
+        parent::set_class($class);
 	}
 }
